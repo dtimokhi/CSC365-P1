ce509a2bf77dc41724d79a7d71cf6d52ed400b70
Hanson <hanson4a4@gmail.com>
2017-09-16 17:19:30 -0700

    Comment schoolSearch.py

--- schoolSearch.py
+++ schoolSearch.py
@@ -29,64 +29,91 @@ def runSearch():
    q=0
    # Start running
    while(q==0):
+      # Read User Input
       choice = input("")
+      # Cast input to lower case
       choice = choice.lower()
+      # Set storage variables
       first = ""
       second = ""
       third = ""
-      thirdLet = ""
+      # Check if anything was entered
       if(len(choice)>0):
-         first=choice.split()[0]
-         firstLet = first[0]
-         if(len(choice.split()) > 1):
-            second=choice.split()[1]
-            secondLet = second[0]
-            if(len(choice.split()) > 2):
-               third=choice.split()[2]
-               thirdLet =third[0]  
+         # Split the line
+         split=choice.split()
+         # Find length
+         lSplit = len(split)
+         # Get first value
+         first = split[0]
+         # Check length to continue
+         if(lSplit > 1):
+            # Read in second value
+            second = split[1]
+            # Check length to continue
+            if(lSplit > 2):
+               # Read in third value
+               third=split[2]
+
+      ### If statements to check command input ###
+      
+      # Student command
       if(first == "s:" or first == "student:"):
          print("Student")
+      # Teacher command
       if(first == "teacher:" or first == "t:"):
+         # Separator for .join()
          s = ","
          try:
             for name in getTeacher(studentList, second):
                print(s.join(name))
          except:
             continue
+      # Bus command
       if(first == "bus:" or first == "b:"):
+         # Separator for .join()
          s = ","
          try:
             for name in getBus(studentList, int(second)):
                print(s.join(name))
          except:
             continue
+      # Grade command
       if(first == "grade:" or first == "g:"):
+         # Separator for .join()
          s = ","
          try:
-             if(thirdLet == ""):
-                 for name in getGradeSearch(studentList, int(second)):
-                     print(s.join(name))
-                 else:
-                   print(s.join(getGradeSearch(studentList, int(second), third)))
+            if(third == ""):
+               for name in getGradeSearch(studentList, int(second)):
+                  print(s.join(name))
+               else:
+                  print(s.join(getGradeSearch(studentList, int(second), third)))
          except:
-             continue
+            continue
+      # Average command
       if((first == "average:" or first == "a:")):
          try:
             tmpList = getAverage(studentList, int(second))
             print(str(tmpList[0]) + "," + str(tmpList[1]))
          except:
             continue
+      # Info command
       if(first == "i" or first == "info"):
          try:
             for grade in getInfo(studentList):
-              print(str(grade[0]) + ": " + str(grade[1]))
+               print(str(grade[0]) + ": " + str(grade[1]))
          except:
             continue
+      # Quit command
       if(first == "q" or first== "quit"):
+         # Set q to stop value
          q=1
       else:
+         # Keep q at running value
          q=0
 
+      ### End of command statements ###
+
+# Issue run command
 runSearch()
 
 
